b9e0154d0bd955a6472da50ffd0beac5511ebe14
anon2014 <singh.anu@gmail.com>
2014-06-24 13:41:30 -0700

    update

--- src/NullGraphException.java
+++ src/NullGraphException.java
@@ -2,22 +2,18 @@
 public class NullGraphException extends Exception {
 
 	public NullGraphException() {
-		// TODO Auto-generated constructor stub
 	}
 
 	public NullGraphException(String message) {
 		super(message);
-		// TODO Auto-generated constructor stub
 	}
 
 	public NullGraphException(Throwable cause) {
 		super(cause);
-		// TODO Auto-generated constructor stub
 	}
 
 	public NullGraphException(String message, Throwable cause) {
 		super(message, cause);
-		// TODO Auto-generated constructor stub
 	}
 
 }
